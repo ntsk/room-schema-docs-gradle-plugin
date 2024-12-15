21cd9fdfcb857977c3460f7d6d3d299a24e7a192
ntsk <ntsk@ntsk.jp>
2024-12-15 16:15:42 +0900

    [sample] delete ui

--- sample/src/main/kotlin/jp/ntsk/room/schema/docs/sample/ui/MainScreen.kt
+++ sample/src/main/kotlin/jp/ntsk/room/schema/docs/sample/ui/MainScreen.kt
@@ -1,32 +1,7 @@
 package jp.ntsk.room.schema.docs.sample.ui
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import jp.ntsk.room.schema.docs.sample.ui.taskdetail.TaskDetailScreen
-import jp.ntsk.room.schema.docs.sample.ui.tasks.TasksScreen
 
 @Composable
 fun MainScreen() {
-    val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = Screens.Tasks,
-    ) {
-        composable<Screens.Tasks> {
-            TasksScreen(
-                onClickFab = {
-                    navController.navigate(Screens.TaskDetail(0))
-                },
-                onClickTask = { task ->
-                    navController.navigate(Screens.TaskDetail(task.id))
-                }
-            )
-        }
-
-        composable<Screens.TaskDetail> {
-            TaskDetailScreen(onClickArrowBack = { navController.popBackStack() })
-        }
-    }
 }
