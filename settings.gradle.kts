a95644f303beb4573368af009da7f55e7eb16321
ntsk <ntsk@ntsk.jp>
2026-03-05 02:34:47 +0900

    Use composite build for plugin resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,6 @@
 pluginManagement {
+    includeBuild("./plugin")
     repositories {
-        if (System.getenv("PLUGIN_ONLY")?.toBoolean() != true) {
-            mavenLocal()
-        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -17,16 +15,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        if (System.getenv("PLUGIN_ONLY")?.toBoolean() != true) {
-            mavenLocal()
-        }
         google()
         mavenCentral()
     }
 }
 
 rootProject.name = "room-schema-docs-gradle-plugin"
-if (System.getenv("PLUGIN_ONLY")?.toBoolean() != true) {
-    include(":sample")
-}
-include(":plugin")
+include(":sample")
